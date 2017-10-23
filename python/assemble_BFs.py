26248348f39ae35333f5ba1e7c008485de9f1c48
Ross Johnstone <rhjohnstone@gmail.com>
2017-10-23 15:59:10 +0100

    Plot BFs against M2 best Hill

--- python/assemble_BFs.py
+++ python/assemble_BFs.py
@@ -4,6 +4,7 @@ import itertools as it
 import os
 import argparse
 import sys
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 
@@ -23,6 +24,9 @@ BFs = np.zeros((30, 7))
 best_params_m1 = {}
 best_params_m2 = {}
 best_params = [best_params_m1, best_params_m2]
+
+best_m2_hills = []
+all_BFs = []
 
 for i, j in drugs_channels_idx:
 
@@ -44,6 +48,9 @@ for i, j in drugs_channels_idx:
         print "{} + {}: B12 = {}".format(drug, channel, BFs[i, j])
         print "M1 best fit: {}".format(best_params[0][(i,j)])
         print "M2 best fit: {}".format(best_params[1][(i,j)])
+        
+    all_BFs.append(BFs[i, j])
+    best_m2_hills.append(best_params[1][(i,j)])
     
 max_idx = np.unravel_index(np.argmax(BFs), (30,7))
 min_idx = np.unravel_index(np.argmin(BFs), (30,7))
@@ -52,3 +59,9 @@ print "max B12: {}, {} + {}".format(BFs[max_idx], dr.drugs[max_idx[0]], dr.chann
 print "max B21: {}, {} + {}".format(1./BFs[min_idx], dr.drugs[min_idx[0]], dr.channels[min_idx[1]])
 
 print "B21 > B12 in {} cases".format(np.sum(BFs<1))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(best_m2_hills, all_BFs)
+plt.show(block=True)
+
